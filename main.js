d321e516673ce4f0de166ad345f719547ea4aa5a
Jenny <jenny@playgroundinc.com>
2019-12-04 16:12:23 -0500

    So basic

---
+++ main.js
@@ -0,0 +1,60 @@
+$(function () {
+  const playBtn = document.getElementById('autoplay');
+  const progressBar = document.getElementById('progressbar');
+
+  const docHeight = document.documentElement.scrollHeight;
+  const windowHeight = window.innerHeight;
+  const calcHeight = docHeight - windowHeight;
+
+  let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+  let percentage = (scrollTop / calcHeight) * 100;
+  progressBar.style.width = `${percentage}%`;
+  progressBar.style.transition = 'width 0.1s ease';
+
+  let scrollInterval;
+  let playing = false;
+
+  window.onscroll = function () {
+    // Get current scroll position
+    scrollTop = window.scrollY || document.documentElement.scrollTop;
+    // Update current scroll percentage
+    percentage = (scrollTop / calcHeight) * 100;
+    // Set width of progress bar
+    progressBar.style.width = `${percentage}%`;
+
+    if (percentage === 100) {
+      playBtn.innerHTML = 'Return to Top';
+      playing = false;
+      clearInterval(scrollInterval);
+    }
+
+  };
+
+  playBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (percentage === 100) {
+      playBtn.innerHTML = 'Play';
+      window.scrollTo(0, 0);
+      percentage = 0;
+    } else {
+      if (playing) {
+        playBtn.innerHTML = 'Play';
+        playing = false;
+        clearInterval(scrollInterval);
+      } else {
+        playing = true;
+        playBtn.innerHTML = 'Pause';
+        scrollInterval = setInterval(
+          function () {
+            window.requestAnimationFrame(function () {
+              window.scrollBy(0, 10);
+            });
+          }, 10);
+      }
+    }
+
+  });
+});
+
+    // TODO: Add resize listeners
